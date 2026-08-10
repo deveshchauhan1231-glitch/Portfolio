f1dfc4db6e2c1a407f1db763bc4aab1d92d5fa22
Devesh Chauhan <deveshchauhan1.2.3.1@gmail.com>
2026-08-10 23:32:30 +0530

    update css for cursor splash effect

--- src/App.jsx
+++ src/App.jsx
@@ -71,18 +71,7 @@ const projectBullets = {
 
 function App() {
 
-  <SplashCursor
-  DENSITY_DISSIPATION={3.5}
-  VELOCITY_DISSIPATION={2}
-  PRESSURE={0.1}
-  CURL={3}
-  SPLAT_RADIUS={0.2}
-  SPLAT_FORCE={6000}
-  COLOR_UPDATE_SPEED={10}
-  SHADING
-  RAINBOW_MODE={false}
-  COLOR="#EAB308"
-/>
+  
   const [menuOpen, setMenuOpen] = useState(false);
   const [expandedProject, setExpandedProject] = useState(null);
 
@@ -104,7 +93,20 @@ function App() {
   const closeMenu = () => setMenuOpen(false);
 
   return (
+    
     <main className="site-shell">
+      <SplashCursor
+  DENSITY_DISSIPATION={3.5}
+  VELOCITY_DISSIPATION={2}
+  PRESSURE={0.1}
+  CURL={3}
+  SPLAT_RADIUS={0.2}
+  SPLAT_FORCE={6000}
+  COLOR_UPDATE_SPEED={10}
+  SHADING
+  RAINBOW_MODE={false}
+  COLOR="#EAB308"
+/>
       <div className="ambient ambient-one" />
       <div className="ambient ambient-two" />
 
